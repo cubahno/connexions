4637a65739080389a87e2dc7f448554cfc9c7c61
cubahno <igor.statler@gmail.com>
2023-08-03 21:01:00 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,32 @@
+module github.com/cubahno/xs
+
+go 1.20
+
+require (
+	github.com/dprotaso/go-yit v0.0.0-20220510233725-9ba8df137936 // indirect
+	github.com/getkin/kin-openapi v0.118.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/swag v0.19.5 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/invopop/yaml v0.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/labstack/echo/v4 v4.11.1 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/pb33f/libopenapi v0.9.7 // indirect
+	github.com/perimeterx/marshmallow v1.1.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/vmware-labs/yaml-jsonpath v0.3.2 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ loaders.go
@@ -0,0 +1,70 @@
+package xs
+
+import (
+    "github.com/getkin/kin-openapi/openapi3"
+    "github.com/labstack/echo/v4"
+    "net/http"
+    "regexp"
+    "strings"
+)
+
+var routePlaceholders = regexp.MustCompile("/:([^/]+)")
+var openAPIPlaceholders = regexp.MustCompile("/{([^/]+)}")
+
+func LoadOpenAPI(serviceName, filePath string, router *echo.Router) error {
+    loader := openapi3.NewLoader()
+    doc, err := loader.LoadFromFile(filePath)
+    if err != nil {
+        return err
+    }
+
+    prefix := ""
+    if serviceName != "" {
+        prefix = "/" + serviceName
+    }
+
+    for resName, pathItem := range doc.Paths {
+        for method, _ := range pathItem.Operations() {
+            path := openAPIPlaceholders.ReplaceAllString(prefix+resName, "/:$1")
+            router.Add(method, path, createHandler(prefix, doc))
+        }
+    }
+    return nil
+}
+
+func createHandler(prefix string, doc *openapi3.T) func(c echo.Context) error {
+    return func(c echo.Context) error {
+        resourceName := strings.Replace(c.Path(), prefix, "", 1)
+        resourceName = routePlaceholders.ReplaceAllString(resourceName, "/{$1}")
+        paths := doc.Paths[resourceName]
+
+        currentMethod := c.Request().Method
+        var operation *openapi3.Operation
+
+        if currentMethod == http.MethodGet {
+            operation = paths.Get
+        } else if currentMethod == http.MethodPost {
+            operation = paths.Post
+        } else if currentMethod == http.MethodPut {
+            operation = paths.Put
+        } else if currentMethod == http.MethodDelete {
+            operation = paths.Delete
+        } else if currentMethod == http.MethodOptions {
+            operation = paths.Options
+        } else if currentMethod == http.MethodHead {
+            operation = paths.Head
+        } else if currentMethod == http.MethodPatch {
+            operation = paths.Patch
+        } else if currentMethod == http.MethodTrace {
+            operation = paths.Trace
+        } else {
+            return c.NoContent(http.StatusMethodNotAllowed)
+        }
+
+        return openAPIHandler(c, operation)
+    }
+}
+
+func openAPIHandler(c echo.Context, operation *openapi3.Operation) error {
+    return c.JSON(http.StatusOK, map[string]string{"hallo": "welt! " + operation.OperationID})
+}
