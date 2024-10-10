fbb83cf3c0dc91e74b64d34792efa8a660d37fab
Igor Hit <126671023+cubahno@users.noreply.github.com>
2024-10-10 19:29:37 +0200

    add base middleware tests (#41)

---
+++ testdata/callbacks/foo.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "encoding/json"
+    "github.com/cubahno/connexions_plugin"
+)
+
+func Foo(resource *connexions_plugin.RequestedResource) ([]byte, error) {
+    s := map[string]any{
+        "foo": "bar",
+    }
+    res, _ := json.Marshal(s)
+    return res, nil
+}
