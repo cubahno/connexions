1ed07998d923735c7efeed01137c79096313cac0
Igor Hittrich <igor.hittrich@wolt.com>
2026-02-18 21:28:37 +0100

    fixlint

--- cmd/api/handler_helpers.go
+++ cmd/api/handler_helpers.go
@@ -53,42 +53,42 @@ func renderSchema(s *schema.Schema) string {
 	}
 
 	var sb strings.Builder
-	sb.WriteString(fmt.Sprintf("&%s{\n", schemaInit))
+	fmt.Fprintf(&sb, "&%s{\n", schemaInit)
 
 	if s.Type != "" {
-		sb.WriteString(fmt.Sprintf("\t\tType: %q,\n", s.Type))
+		fmt.Fprintf(&sb, "\t\tType: %q,\n", s.Type)
 	}
 
 	if s.Format != "" {
-		sb.WriteString(fmt.Sprintf("\t\tFormat: %q,\n", s.Format))
+		fmt.Fprintf(&sb, "\t\tFormat: %q,\n", s.Format)
 	}
 
 	if s.Pattern != "" {
-		sb.WriteString(fmt.Sprintf("\t\tPattern: %q,\n", s.Pattern))
+		fmt.Fprintf(&sb, "\t\tPattern: %q,\n", s.Pattern)
 	}
 
 	if s.Minimum != nil {
-		sb.WriteString(fmt.Sprintf("\t\tMinimum: ptr(%v),\n", *s.Minimum))
+		fmt.Fprintf(&sb, "\t\tMinimum: ptr(%v),\n", *s.Minimum)
 	}
 
 	if s.Maximum != nil {
-		sb.WriteString(fmt.Sprintf("\t\tMaximum: ptr(%v),\n", *s.Maximum))
+		fmt.Fprintf(&sb, "\t\tMaximum: ptr(%v),\n", *s.Maximum)
 	}
 
 	if s.MinLength != nil {
-		sb.WriteString(fmt.Sprintf("\t\tMinLength: ptr(int64(%v)),\n", *s.MinLength))
+		fmt.Fprintf(&sb, "\t\tMinLength: ptr(int64(%v)),\n", *s.MinLength)
 	}
 
 	if s.MaxLength != nil {
-		sb.WriteString(fmt.Sprintf("\t\tMaxLength: ptr(int64(%v)),\n", *s.MaxLength))
+		fmt.Fprintf(&sb, "\t\tMaxLength: ptr(int64(%v)),\n", *s.MaxLength)
 	}
 
 	if s.MinItems != nil {
-		sb.WriteString(fmt.Sprintf("\t\tMinItems: ptr(int64(%v)),\n", *s.MinItems))
+		fmt.Fprintf(&sb, "\t\tMinItems: ptr(int64(%v)),\n", *s.MinItems)
 	}
 
 	if s.MaxItems != nil {
-		sb.WriteString(fmt.Sprintf("\t\tMaxItems: ptr(int64(%v)),\n", *s.MaxItems))
+		fmt.Fprintf(&sb, "\t\tMaxItems: ptr(int64(%v)),\n", *s.MaxItems)
 	}
 
 	if len(s.Enum) > 0 {
@@ -100,24 +100,24 @@ func renderSchema(s *schema.Schema) string {
 			// Handle different types of enum values
 			switch v := e.(type) {
 			case string:
-				sb.WriteString(fmt.Sprintf("%q", v))
+				fmt.Fprintf(&sb, "%q", v)
 			case bool, int, int64, float64:
-				sb.WriteString(fmt.Sprintf("%v", v))
+				fmt.Fprintf(&sb, "%v", v)
 			default:
-				sb.WriteString(fmt.Sprintf("%q", fmt.Sprint(v)))
+				fmt.Fprintf(&sb, "%q", fmt.Sprint(v))
 			}
 		}
 		sb.WriteString("},\n")
 	}
 
 	if s.Items != nil {
-		sb.WriteString(fmt.Sprintf("\t\tItems: %s,\n", renderSchema(s.Items)))
+		fmt.Fprintf(&sb, "\t\tItems: %s,\n", renderSchema(s.Items))
 	}
 
 	if len(s.Properties) > 0 {
-		sb.WriteString(fmt.Sprintf("\t\tProperties: map[string]*%s{\n", schemaInit))
+		fmt.Fprintf(&sb, "\t\tProperties: map[string]*%s{\n", schemaInit)
 		for name, prop := range s.Properties {
-			sb.WriteString(fmt.Sprintf("\t\t\t%q: %s,\n", name, renderSchema(prop)))
+			fmt.Fprintf(&sb, "\t\t\t%q: %s,\n", name, renderSchema(prop))
 		}
 		sb.WriteString("\t\t},\n")
 	}
@@ -128,13 +128,13 @@ func renderSchema(s *schema.Schema) string {
 			if i > 0 {
 				sb.WriteString(", ")
 			}
-			sb.WriteString(fmt.Sprintf("%q", r))
+			fmt.Fprintf(&sb, "%q", r)
 		}
 		sb.WriteString("},\n")
 	}
 
 	if s.AdditionalProperties != nil {
-		sb.WriteString(fmt.Sprintf("\t\tAdditionalProperties: %s,\n", renderSchema(s.AdditionalProperties)))
+		fmt.Fprintf(&sb, "\t\tAdditionalProperties: %s,\n", renderSchema(s.AdditionalProperties))
 	}
 
 	if s.Nullable {
